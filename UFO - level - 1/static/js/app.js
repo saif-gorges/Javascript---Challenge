2c892504ae3f31a0b2732637e8e740c92bf2ff0b
Saif Gorges <69221324+saif-gorges@users.noreply.github.com>
2021-01-09 20:31:10 -0500

    Update app.js

--- UFO - level - 1/static/js/app.js
+++ UFO - level - 1/static/js/app.js
@@ -1,4 +1,18 @@
-// from data.js
+// Assign the data from `data.js` to a descriptive variable
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(tableData)
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Append a row for each value
+tableData.forEach(function(sighting) {
+var row = tbody.append("tr");
+Object.entries(sighting).forEach(function([key, value]) {
+
+// Append a cell to the row for each value
+var cell = row.append("td");
+cell.text(value);
+});
+});
