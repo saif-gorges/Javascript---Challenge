bd2f4bdb691b29b23201af15428fe7e85f4e7d49
Saif Gorges <69221324+saif-gorges@users.noreply.github.com>
2021-01-11 14:44:46 -0500

    Added function function

--- UFO - level - 1/static/js/app.js
+++ UFO - level - 1/static/js/app.js
@@ -3,10 +3,8 @@ var tableData = data;
 
 console.log(tableData)
 
-// Select the button
-var button = d3.select("#button");
-
-// Select the form
+// Select the button & form
+var button = d3.select("#filter-btn");
 var form = d3.select("#form");
 
 // Create event handlers 
@@ -15,31 +13,31 @@ form.on("submit",runEnter);
 
 // Complete the event handler function for the form
 
-function runEnter() {  
-// Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");  
-  
-  // Get the value property of the input button
-  var inputValue = inputElement.property("value");  
-  console.log(inputValue);
-  console.log(people);  
-  var filteredData = people.filter(person => person.bloodType === inputValue);  
-  console.log(filteredData);  
-  
-  // BONUS: Calculate summary statistics for the age field of the filtered data  // First, create an array with just the age values
-  var ages = filteredData.map(person => person.age); 
-};
-
 // Get a reference to the table body
-var tbody = d3.select("tbody");
 
-// Append a row for each value
-tableData.forEach(function(sighting) {
-var row = tbody.append("tr");
-Object.entries(sighting).forEach(function([key, value]) {
+function runEnter() {
 
-// Append a cell to the row for each value
-var cell = row.append("td");
-cell.text(value);
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the filter element & assign variable
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    // Filter
+    var filterData = tableData.filter(day => day.datetime === inputValue);
+
+    // Select table
+    var tbody = d3.select("tbody");
+
+    // Append a row for each value
+    filterData.forEach(function(sighting) {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key,value]) {
+
+    // Append a cell to the row for each value
+    var cell = row.append("td");
+    cell.text(value);
 });
 });
+};
